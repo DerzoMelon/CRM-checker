d482d07fb3d89cfc130a788d28c4fb5b6c5ae1ca
Николай Кохтенко <avdeeviluha@gmail.com>
2023-10-22 14:46:01 +0300

    Fixed compare printing, added open methods

--- CRM test/Base.cs
+++ CRM test/Base.cs
@@ -9,9 +9,30 @@ public class Base
 {
     private Workbook _wb;
     private Dictionary<string, Product> _shop = new Dictionary<string, Product>();
-    public Base(string file)
+
+    public void Open_Base(string Path)
     {
-        _wb = new Workbook(file);
+        _wb = new Workbook(Path);
+        Worksheet ws = _wb.Worksheets[0];
+        int rows = ws.Cells.MaxDataRow + 1;
+        for (int i = 0; i < rows; i++)
+        {
+            if (!_shop.ContainsKey(ws.Cells[i, 0].Value.ToString()))
+            {
+                Product _new = new Product(ws.Cells[i, 0].Value.ToString(), Int32.Parse(ws.Cells[i, 1].Value.ToString()));
+                _shop.Add(ws.Cells[i, 0].Value.ToString(), _new);
+            }
+            else
+            {
+                // Product _new = new Product(ws.Cells[i, 6].Value.ToString() + "${i}", Int32.Parse(ws.Cells[i, 29].Value.ToString()));
+                //  _shop.Add(ws.Cells[i, 6].Value.ToString(), _new);
+            }
+        }
+    }
+
+    public void Open_Shop(string Path) 
+    {
+        _wb = new Workbook(Path);
         Worksheet ws = _wb.Worksheets[0];
         int rows = ws.Cells.MaxDataRow + 1;
         for (int i = 1; i < rows; i++)
@@ -28,6 +49,7 @@ public class Base
             }
         }
     }
+
     public void read()
     { 
        
@@ -44,14 +66,15 @@ public class Base
         }
     }
 
-    public async void compare(Base @base)
+    public async void compare(Base base_shop)
     {
         Dictionary<string, Product> res = new Dictionary<string, Product>();
         foreach(var item in _shop) 
         {
-            if(@base.GetProduct(item.Value.getName()).getAmount()/2 > item.Value.getAmount());
+            if (item.Value.getAmount()/2 > base_shop.GetProduct(item.Value.getName()).getAmount())
             {
-                Product _new = new Product(item.Value.getName(), @base.GetProduct(item.Value.getName()).getAmount() - item.Value.getAmount());
+                Product _new = new Product(item.Value.getName(), item.Value.getAmount() - base_shop.GetProduct(item.Value.getName()).getAmount());
+                res.Add(item.Value.getName(), _new);
             }
         }
 
@@ -59,7 +82,7 @@ public class Base
         {
             using (StreamWriter writer = new StreamWriter("Result.txt", true))
             {
-                await writer.WriteAsync(item.Value.printIn());
+                await writer.WriteAsync(item.Value.printIn() + "\n");
             }
         }
     }
